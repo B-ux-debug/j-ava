3abe7ad10d0889b0f52ee1b4ea06335fa2cd01f7
vitão dos -3 cm <140471002+B-ux-debug@users.noreply.github.com>
2024-03-12 14:32:27 -0300

    Add files via upload

---
+++ type-number.js
@@ -0,0 +1,11 @@
+ //tipo number
+
+ const meunumero = 3;
+
+ const primeiroNumero = 1;
+ const segundoNumero = 4;
+
+ const operacaomatematica = primeiroNumero *
+  segundoNumero
+
+  console.log(operacaomatematica)
